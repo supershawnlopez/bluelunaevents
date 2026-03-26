ca16c8ed9724057573f9190689310103f85a8f99
supershawnlopez <shawnlopez@me.com>
2026-03-25 17:22:09 -0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,12 +1,1 @@
-window.BLUE_LUNA_CONFIG = {
-  stripe: {
-    essential: '',
-    signature: '',
-    luxe: ''
-  },
-  contact: {
-    phoneDisplay: '(520) 222-6142',
-    phoneHref: 'tel:+15202226142',
-    email: 'monica@bluelunaevents.com'
-  }
-};
+const stripeLinks={essential:"#",signature:"#",luxe:"#"};
